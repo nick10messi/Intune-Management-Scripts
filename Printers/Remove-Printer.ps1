59877d2ab4eec0f8f29c542a9720008ef97b7665
Nick Kok <nick.kok@conclusion.nl>
2024-10-03 09:48:23 +0200

    -

---
+++ Printers/Remove-Printer.ps1
@@ -0,0 +1,20 @@
+<#
+powershell.exe -executionpolicy bypass -file .\Remove-Printer.ps1 -PrinterName "Canon Printer Upstairs"
+
+.Example
+.\Remove-Printer.ps1 -PrinterName "Canon Printer Upstairs"
+#>
+
+$PrinterName = "OneNote (Desktop)"
+
+Try {
+    #Remove Printer
+    $PrinterExist = Get-Printer -Name $PrinterName -ErrorAction SilentlyContinue
+    if ($PrinterExist) {
+        Remove-Printer -Name $PrinterName -Confirm:$false
+    }
+}
+Catch {
+    Write-Warning "Error removing Printer"
+    Write-Warning "$($_.Exception.Message)"
+}
