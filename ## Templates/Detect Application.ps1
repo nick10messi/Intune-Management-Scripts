b2312e84ac62d44e31555816e3726a086ccbde23
Nick Kok <nick.kok@conclusion.nl>
2024-10-04 12:17:43 +0200

    -#

--- ## Templates/Detect Application.ps1
+++ ## Templates/Detect Application.ps1
@@ -1,8 +1,10 @@
 $app = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -like "APPNAME WMI"} | Select-Object Name -ExpandProperty Name
 
 if (($app) -eq "APPNAME WMI") {
+    Write-Output "APPNAME WMI is geïnstalleerd, doorgaan naar remediation"
     exit 1
 }
 else {
+    Write-Output "APPNAME WMI is niet geïnstalleerd"
     exit 0
 }
