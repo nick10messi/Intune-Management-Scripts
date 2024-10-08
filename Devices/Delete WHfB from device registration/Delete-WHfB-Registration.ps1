29d1c1e94ad4b3ba802cf955545f1f07e6efcc72
Nick Kok <nick.kok@conclusion.nl>
2024-10-08 16:20:52 +0200

    -

---
+++ Devices/Delete WHfB from device registration/Delete-WHfB-Registration.ps1
@@ -0,0 +1,3 @@
+# PS-script in Intune needs to be run in the User context
+certutil /deletehellocontainer
+shutdown /r /f /t 120 /c "Uw computer dient opnieuw opgestart te worden. Groet Jermo Buijs (KleinGeluk)"
