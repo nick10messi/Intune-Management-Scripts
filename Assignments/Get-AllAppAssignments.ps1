8e6a649c4eee5e2a1418558b5fc0e7ff14991e2b
Nick Kok <nick.kok@conclusion.nl>
2024-10-25 16:37:38 +0200

    -

--- Assignments/Get-AllAppAssignments.ps1
+++ Assignments/Get-AllAppAssignments.ps1
@@ -1,26 +1,11 @@
-# Get Intune Apps and their Assignments
-$apps = Get-MgDeviceAppManagementMobileApp | Select-Object Id, DisplayName
+# Connect to MS Graph
+Connect-MgGraph -Scopes "DeviceManagementApps.Read.All,Group.Read.All" -NoWelcome
 
-$results = foreach ($app in $apps) {
-    $assignments = Get-MgDeviceAppManagementMobileAppAssignment -MobileAppId $app.Id
-    Write-Output $assignments
-    foreach ($assignment in $assignments) {
-        if ($groupId -eq 'acacacac-9df4-4c7d-9d50-4ef0226f57a9_1_0') {
-            $groupName = 'All Users'
-        } elseif ($groupId -eq 'adadadad-808e-44e2-905a-0b7873a8a531_1_0') {
-            $groupName = 'All Devices'
-        } else {
-            $group = Get-MgGroup -GroupId $groupId
-            $groupName = $group.DisplayName
-        }
-        [PSCustomObject]@{
-            'App ID'        = $app.Id
-            'Displayname'   = $app.DisplayName
-            'Intent'        = $assignment.Intent
-            'Groupname'     = $groupName
-        }
-    }
-}
+# CSV location
+$CSV_location = "$env:USERPROFILE\Downloads\IntuneAppAssignments.csv"
 
-# Export to CSV
-$results | Export-Csv -Path "$env:USERPROFILE\Downloads\IntuneAppsAssignments.csv" -NoTypeInformation
+# Get the app IDs and display names of all Intune apps in a single query
+$appInfo = Get-MgDeviceAppManagementMobileApp | Select-Object Id, DisplayName
+
+# Export to CSV, ensuring only ID and DisplayName columns are present
+$appInfo | Export-Csv -Path $CSV_location -NoTypeInformation -Force
