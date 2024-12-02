e7725adb13dfd67ed3a5f7600d329479ed6454f0
Nick Kok <nick.kok@conclusion.nl>
2024-12-02 05:56:20 +0100

    -

--- Devices/Windows Hello for Business/Delete WHfB from device registration/Delete-WHfB-Registration.ps1
+++ Devices/Windows Hello for Business/Delete WHfB from device registration/Delete-WHfB-Registration.ps1
@@ -1,3 +1,3 @@
 # PS-script in Intune needs to be run in the User context
 certutil /deletehellocontainer
-shutdown /r /f /t 120 /c "Uw computer dient opnieuw opgestart te worden. Groet Jermo Buijs (KleinGeluk)"
+shutdown /r /f /t 900 /c "Uw computer dient binnen 15 minuten opnieuw opgestart te worden."
---
+++ EPM/Admin - Get EPM requests.ps1
@@ -0,0 +1,24 @@
+﻿# Connect to MS Graph
+Connect-MgGraph -Scopes "DeviceManagementConfiguration.Read.All" -NoWelcome
+
+# Retrieve data from Graph API
+$epmRequests = Invoke-MgGraphRequest -Method Get -Uri 'https://graph.microsoft.com/beta/deviceManagement/elevationRequests'
+
+$OutputPath = (Join-Path $Env:USERPROFILE\Downloads "EPM-Requests.csv")
+
+# Loop through all EPM elevation requests and export this data to a CSV-file
+$epmRequests.Value | ForEach-Object {
+    [PSCustomObject]@{        
+        Aanvrager = $_.requestedByUserPrincipalName
+        Aanvraagdatum = $_.requestCreatedDateTime
+        Reden = $_.requestJustification
+        Status = $_.status
+        Reviewer = $_.reviewCompletedByUserPrincipalName
+        Beoordelingsdatum = $_.reviewCompletedDateTime
+        Beoordelingsreden = $_.reviewerJustification
+        Bestandsnaam = $_.applicationDetail.fileName
+        Bestandspad = $_.applicationDetail.filePath
+        Uitgever = $_.applicationDetail.publisherName
+        Versie = $_.applicationDetail.productVersion
+    }
+} | Export-Csv -Path $OutputPath -NoTypeInformation
