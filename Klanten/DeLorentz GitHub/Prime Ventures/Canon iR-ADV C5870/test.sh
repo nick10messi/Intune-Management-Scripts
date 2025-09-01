319c2e0b40eeb30d89b30e138464638e0e3e8dac
Nick Kok <nick.kok@live.nl>
2025-09-01 11:10:29 +0200

    -

---
+++ Klanten/DeLorentz GitHub/Prime Ventures/Canon iR-ADV C5870/test.sh
@@ -0,0 +1,1 @@
+sudo /usr/sbin/lpadmin -p canonc5870 -D "Canon iR-ADV C5870" -E -v "ipp://10.10.10.200/ipp/print" -P "/Library/Printers/PPDs/Contents/Resources/CNPZUIR5870CZU.ppd.gz" -o printer-is-shared=false && sudo /usr/bin/lpoptions -d canonc5870
