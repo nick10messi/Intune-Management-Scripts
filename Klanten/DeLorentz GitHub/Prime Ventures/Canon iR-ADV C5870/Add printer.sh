65b7271ec0bfb1d783a0fa103b3ec2b5064a9957
Nick Kok <nick.kok@live.nl>
2025-09-01 11:13:55 +0200

    -

--- Klanten/DeLorentz GitHub/Prime Ventures/Canon iR-ADV C5870/Add printer.sh
+++ Klanten/DeLorentz GitHub/Prime Ventures/Canon iR-ADV C5870/Add printer.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-# Printergegevens
-PRINTER_NAME="Canon_IRADV_C5870"
-PRINTER_IP="10.10.10.200"
-PPD_FILE="CNPZUIR5870CZU.ppd.gz"
-PPD_PATH="/Library/Printers/PPDs/Contents/Resources/${PPD_FILE}"
+/usr/sbin/lpadmin -p canonc5870 \
+  -D "Canon iR-ADV C5870" \
+  -E \
+  -v "ipp://10.10.10.200/ipp/print" \
+  -P "/Library/Printers/PPDs/Contents/Resources/CNPZUIR5870CZU.ppd.gz" \
+  -o printer-is-shared=false
 
-# Controleren of het PPD-bestand bestaat
-if [ ! -f "$PPD_PATH" ]; then
-    echo "Canon PPD-bestand niet gevonden: $PPD_PATH"
-    exit 1
-fi
-
-# Printer toevoegen via IPP
-/usr/sbin/lpadmin -p "$PRINTER_NAME" \
-        -E \
-        -v "ipp://${PRINTER_IP}/ipp/print" \
-        -P "$PPD_PATH" \
-        -o printer-is-shared=false
-
-# Optioneel: instellen als standaardprinter
-/usr/bin/lpoptions -d "$PRINTER_NAME"
-
-echo "Printer $PRINTER_NAME succesvol toegevoegd."
-exit 0
+/usr/bin/lpoptions -d canonc5870
