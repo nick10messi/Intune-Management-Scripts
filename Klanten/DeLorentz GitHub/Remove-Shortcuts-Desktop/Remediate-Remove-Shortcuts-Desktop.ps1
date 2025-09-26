435685366a96c62f8075442a65960be36c9d1b39
Nick Kok <nick.kok@live.nl>
2025-09-26 16:25:14 +0200

    -

--- Klanten/DeLorentz GitHub/Remove-Shortcuts-Desktop/Remediate-Remove-Shortcuts-Desktop.ps1
+++ Klanten/DeLorentz GitHub/Remove-Shortcuts-Desktop/Remediate-Remove-Shortcuts-Desktop.ps1
@@ -1,8 +1,9 @@
 ###########################################################################
 # Auteur      : Nick Kok
-# Doel        : Verwijdert ongewenste snelkoppelingen van de desktop (gebruiker én publiek)
-# Versie      : 1.0
-# Datum       : 25-09-2025
+# Doel        : Verwijdert ongewenste snelkoppelingen (.lnk en .url) 
+#               van desktop (gebruiker én publiek)
+# Versie      : 1.3
+# Datum       : 26-09-2025
 ###########################################################################
 
 Function Write-Log {
@@ -15,8 +16,19 @@ Function Write-Log {
     Add-Content -Path $LogFile -Value "$TimeStamp [$Level] - $Message"
 }
 
+Function Get-LoggedOnUserDesktopPath {
+    $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI"
+    $SID = (Get-ItemProperty -Path $RegPath -Name LastLoggedOnUserSID).LastLoggedOnUserSID
+    if ($SID) {
+        $UserProfile = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$SID").ProfileImagePath
+        return Join-Path -Path $UserProfile -ChildPath "Desktop"
+    }
+    return $null
+}
+
 Write-Log -Message "Start verwijdering van ongewenste snelkoppelingen." -Level "INFO"
 
+# Zowel .lnk als .url opnemen
 $ShortcutsToRemove = @(
     "Google Chrome.lnk",
     "Microsoft Edge.lnk",
@@ -27,28 +39,36 @@ $ShortcutsToRemove = @(
     "Excel.lnk",
     "Powerpoint.lnk",
     "Uurwerk.lnk",
+    "Uurwerk.url",
     "Yesplan.lnk",
-    "Topdesk.lnk"
+    "Yesplan.url",
+    "Topdesk.lnk",
+    "Topdesk.url",
+    "Maak verbinding met G-schijf.lnk",
+    "Maak verbinding met G-schijf.url"
 )
 
 $DesktopPaths = @(
-    [Environment]::GetFolderPath("Desktop"),
     "$env:PUBLIC\Desktop"
 )
 
 foreach ($Path in $DesktopPaths) {
-    foreach ($Shortcut in $ShortcutsToRemove) {
-        $FullPath = Join-Path -Path $Path -ChildPath $Shortcut
-        if (Test-Path $FullPath) {
-            try {
-                Remove-Item -Path $FullPath -Force
-                Write-Log -Message "Verwijderd: $FullPath" -Level "INFO"
-            } catch {
-                Write-Log -Message "Fout bij verwijderen van $FullPath - $_" -Level "ERROR"
+    if (-not [string]::IsNullOrWhiteSpace($Path) -and (Test-Path $Path)) {
+        foreach ($Shortcut in $ShortcutsToRemove) {
+            $FullPath = Join-Path -Path $Path -ChildPath $Shortcut
+            if (Test-Path $FullPath) {
+                try {
+                    Remove-Item -Path $FullPath -Force
+                    Write-Log -Message "Verwijderd: $FullPath" -Level "INFO"
+                } catch {
+                    Write-Log -Message "Fout bij verwijderen van $FullPath - $_" -Level "ERROR"
+                }
+            } else {
+                Write-Log -Message "Niet gevonden (overslaan): $FullPath" -Level "INFO"
             }
-        } else {
-            Write-Log -Message "Niet gevonden (overslaan): $FullPath" -Level "INFO"
         }
+    } else {
+        Write-Log -Message "Ongeldig pad overgeslagen: '$Path'" -Level "WARN"
     }
 }
 
